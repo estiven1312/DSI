3230c6403869c40091e1718f9b9a14d11aa26ac9
Estiven Hurtado <santosestiven46@gmail.com>
2022-10-30 20:56:44 -0500

    PRIMER COMMIT

---
+++ src/main/java/com/hospital/projecthospital/ProjectHospitalApplication.java
@@ -0,0 +1,13 @@
+package com.hospital.projecthospital;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectHospitalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjectHospitalApplication.class, args);
+	}
+
+}
